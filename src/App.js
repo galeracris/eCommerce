f3793973e2476f712d95dd44fc64a758c7f6b655
galeracris <cristian.galera@gmail.com>
2021-09-01 15:56:01 -0300

    se crearon rutas, estado y contexto de la app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
 // import Producto from '../src/components/Productos/Producto';
 import Navbar from './components/Navbar/Navbar';
-// import ContainerProductos from '../src/components/ContainerProductos/ContainerProductos';
+import ContainerProductos from '../src/components/ContainerProductos/ContainerProductos';
 import CheckoutPage from '../src/components/Checkout/CheckoutPage';
 // import CheckoutCard from './components/Checkout/CheckoutCard';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <CheckoutPage />
-      {/* <CheckoutCard /> */}
-      <Navbar />
-      {/* <ContainerProductos /> */}
-      {/* <Producto /> */}
+      <Navbar/>
+      <Switch>
+        <Route path="/checkout-page">
+          <CheckoutPage/>
+        </Route>
+        <Route path="/">
+          <ContainerProductos />
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
