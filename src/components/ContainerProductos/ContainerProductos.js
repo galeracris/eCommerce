0a5bba5353def01554966570bfb271c3bd4c8d5b
Hernan Case <you@example.com>
2021-10-05 17:15:04 -0300

    fix Imagen producto

--- src/components/ContainerProductos/ContainerProductos.js
+++ src/components/ContainerProductos/ContainerProductos.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
-import { getData } from '../Firebase/index'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Producto from '../Productos/Producto';
+import { getData } from '../../services/firebase';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,31 +12,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 const ContainerProductos = () => {
   const classes = useStyles();
   // 1 estado para mostrar la lista
   const [latas, setLatas] = useState([]);
 
   useEffect(() => {
-
     // 2 PIDO LOS DATOS (async/await)
     const getLatas = async () => {
       // 3 obtener colleccion
-        const latasCollection = collection(getData(), 'latas' );
+      const latasCollection = collection(getData(), 'latas');
 
-        // 4 obtener Snapshot de la lista
-        const latasSnapshot = await getDocs(latasCollection);
+      // 4 obtener Snapshot de la lista
+      const latasSnapshot = await getDocs(latasCollection);
 
-        // 5 obtener datos en forma de json con data()
-        const latasList = latasSnapshot.docs.map(doc => ({
-          id: doc.id,
-          ...doc.data()
-        }));
+      // 5 obtener datos en forma de json con data()
+      const latasList = latasSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
 
-        // 6 setear el estado con la lista
-        console.log(latasList);
-        setLatas(latasList);
+      // 6 setear el estado con la lista
+      setLatas(latasList);
     };
     // ejecutar la funcion asincronica
     getLatas();
@@ -44,17 +41,15 @@ const ContainerProductos = () => {
 
   return (
     <div className={classes.root}>
-    <Grid container spacing={3}>
-    {latas.map(latasItem => (
-
-       <Grid item xs={12} sm={6} md={4} lg={3}>
-       <Producto key={latasItem.id} data={latasItem} />
+      <Grid container spacing={3}>
+        {latas.map((lata) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <Producto key={lata.id} data={lata} />
+          </Grid>
+        ))}
       </Grid>
-
-  ))}
-        </Grid>
-      </div>
-    );
-}
+    </div>
+  );
+};
 
 export default ContainerProductos;
